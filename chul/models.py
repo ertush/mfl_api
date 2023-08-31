eba8c6adfd07113020a6666b8b5f6329ad166077
ertush <erotush77@gmail.com>
2023-08-31 11:16:18 +0300

    modified chul/models.py

--- chul/models.py
+++ chul/models.py
@@ -367,19 +367,23 @@ class ChuUpdateBuffer(AbstractBase):
             raise ValidationError({"__all__": ["Nothing was edited"]})
 
     def update_basic_details(self):
-        basic_details = json.loads(self.basic)
-        if 'status' in basic_details:
-            basic_details['status_id'] = basic_details.get(
-                'status').get('status_id')
-            basic_details.pop('status')
-        if 'facility' in basic_details:
-            basic_details['facility_id'] = basic_details.get(
-                'facility').get('facility_id')
-            basic_details.pop('facility')
-
-        for key, value in basic_details.iteritems():
-            setattr(self.health_unit, key, value)
-        self.health_unit.save()
+        if self.basic:
+            basic_details = json.loads(self.basic)
+            if 'status' in basic_details:
+                basic_details['status_id'] = basic_details.get(
+                    'status').get('status_id')
+                basic_details.pop('status')
+            if 'facility' in basic_details:
+                basic_details['facility_id'] = basic_details.get(
+                    'facility').get('facility_id')
+                basic_details.pop('facility')
+           
+            
+            for key, value in basic_details.iteritems():
+                setattr(self.health_unit, key, value)
+            if 'basic' in basic_details:
+                setattr(self.health_unit, 'facility_id', basic_details.get('basic').get('facility'))
+            self.health_unit.save()
 
     def update_workers(self):
         chews = json.loads(self.workers)
@@ -492,6 +496,7 @@ class ChuUpdateBuffer(AbstractBase):
 
     def __str__(self):
         return self.health_unit.name
+
 
 
 class CHUServiceLink(AbstractBase):
