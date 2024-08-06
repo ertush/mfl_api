804d3080b2b4f7b1087a8f8bb16eac828db48e9a
eric <erotush77@gmail.com>
2024-08-06 10:31:16 +0300

    Added debug statements to line 417 of chul/models.py

--- chul/models.py
+++ chul/models.py
@@ -414,7 +414,7 @@ class CommunityHealthUnit(SequenceMixin, AbstractBase):
         else:
             raise ValidationError(
                 {
-                    "Error!": ["Unable to resolve exact Facility linked to the CHU in DHIS2. Code: {}; Response: {}".format(self.facility.code, r.text())]
+                    "Error!": ["Unable to resolve exact Facility linked to the CHU in DHIS2. [DEBUG] Response: {}".format( r.text())]
                 }
             )
 
